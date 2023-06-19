052cf41959137589f229273cf1edb2e68de826bf
Alejandro-Repizo <alejo.oafr2001@gmail.com>
2023-06-19 13:53:05 -0500

    Created the header and the first section

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import css from './Form.module.css'
 
 export default function Form () {
     return (
-        <section>
-            <form>
+        <aside className={css['content']}>
+            <form className={css['form']}>
                 <label>
-                    <span>Put your link that you would like to short.</span>
-                    <input type="text" placeholder="Shorten link here" />
-                    <button type="submit">Shorten it!</button>
+                    <span className={css['sr-only']}>Put your link that you would like to short.</span>
+                    <input type="text" placeholder="Shorten link here..." />
+                    <button className="button button--big button--square" type="submit">Shorten it!</button>
                 </label>
             </form>
-        </section>
+        </aside>
     )
 
 }
